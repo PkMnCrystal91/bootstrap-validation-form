564c7e79f229e8095603b2d620e9eeea66d6b559
Herbert Gutierrez <dev.gutti1391@gmail.com>
2023-06-03 12:13:13 -0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+#Requirements
+
+- Bootstrap validation
+- Form validation
+
+- Form fields:
+
+codigo String
+description String
+codigo de barra Number
+unit price Number
+stock Number
+proveedor
+stado Boolean (checkbox)
+category (dropdown list)
+
+guardar
+cancelar
---
+++ js/index.js
@@ -0,0 +1,60 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  let forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+/* BARCODE */
+const cardNumber = document.querySelector("#card-number");
+cardNumber.addEventListener("keyup", (e) => {
+  if (!e.target.value) {
+    cardNumberText.innerText = "1234 5678 9101 1121";
+  } else {
+    const valuesOfInput = e.target.value.replaceAll(" ", "");
+
+    if (e.target.value.length > 14) {
+      e.target.value = valuesOfInput.replace(
+        /(\d{4})(\d{4})(\d{4})(\d{0,4})/,
+        "$1 $2 $3 $4"
+      );
+    } else if (e.target.value.length > 9) {
+      e.target.value = valuesOfInput.replace(
+        /(\d{4})(\d{4})(\d{0,4})/,
+        "$1 $2 $3"
+      );
+    } else if (e.target.value.length > 4) {
+      e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+    } else {
+      cardNumberText.innerHTML = valuesOfInput;
+    }
+  }
+});
+
+function mostrarTextoLabel() {
+  var checkbox = document.getElementById("invalidCheck");
+  var textoLabel = document.getElementById("textoLabel");
+
+  if (checkbox.checked) {
+    textoLabel.style.display = "block"; // Mostrar el texto
+  } else {
+    textoLabel.style.display = "none"; // Ocultar el texto
+  }
+}
